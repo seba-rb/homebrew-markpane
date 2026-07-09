3cc7df39056fa75c2a9cba5308c54249a64cf96e
Seba <tatanegon@gmail.com>
2026-07-09 17:08:59 -0400

    release: markpane CLI v0.3.0

--- Formula/markpane.rb
+++ Formula/markpane.rb
@@ -1,9 +1,9 @@
 class Markpane < Formula
   desc "Markpane CLI: smart views, frontmatter queries, and Agent Briefs from the terminal"
   homepage "https://markpane.com"
-  url "https://markpane.com/downloads/markpane-cli-0.2.0.zip"
-  sha256 "c23580a374fa6d602a94e42fd428e12723379bb63f0ed5d84de00a06d9059341"
-  version "0.2.0"
+  url "https://markpane.com/downloads/markpane-cli-0.3.0.zip"
+  sha256 "d92dc7992ea97e4c7b6eeb007c89235ff94bf988810bf75e130672918c3dd157"
+  version "0.3.0"
   license :cannot_represent
 
   depends_on :macos
